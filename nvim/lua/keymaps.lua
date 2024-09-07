8cf4415196d0219793469ac49bccc746fb604c7a
bost367 <24195348+bost367@users.noreply.github.com>
2024-09-07 12:44:16 +0300

    Add colors ti yazi & zellij (#3)
    - add yazi jetbrains theme
    - add zellij theme
    - fix alacritty colours
    - change vim theme
    
    Co-authored-by: Stepan Kruglov <s.kruglov@tinkoff.ru>

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,6 +1,12 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = {noremap = true, silent = true}
 
+-- custom binding
+map("n", "gh", "0", default_opts)
+map("x", "gh", "0", default_opts)
+map("n", "gl", "$", default_opts)
+map("x", "gl", "$", default_opts)
+
 -- neo tree 
 map("n", "<Tab>", ":Neotree toggle<CR>", default_opts)
 map("n", "<c-up>", ":call SwapUp()<CR>", default_opts)
