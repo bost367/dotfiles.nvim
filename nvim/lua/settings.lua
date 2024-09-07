61267a1d74c8d346d56892611f894c8f013ec61a
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2024-09-07 12:44:16 +0300

    Add colors ti yazi & zellij (#3)
    - add yazi jetbrains theme
    - add zellij theme
    - fix alacritty colours
    - change vim theme
    
    Co-authored-by: Stepan Kruglov <s.kruglov@tinkoff.ru>

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,5 +1,6 @@
 local opt = vim.opt 
 local cmd = vim.cmd
+local hi = vim.api.nvim_set_hl
 
 -- fileformats
 opt.encoding='utf-8'
@@ -29,10 +30,35 @@ opt.scrolloff = 5
 opt.number = true
 opt.numberwidth = 2
 
--- colour pallete
-opt.termguicolors = true -- Enable true colors
-cmd.colorscheme("darcula-dark")
-cmd 'hi CursorLine guibg=#404040'
-
 cmd 'syntax enable' -- syntax highlighting
 cmd 'filetype plugin indent on' -- try to recognize filetypes and load rel' plugins
+
+-- colour pallete
+
+local colors = {
+  black    = '#1E2022',
+  blue     = '#375FAD',
+  green    = '#549159',
+  red      = '#ec5f67',
+  cyan     = '#008080',
+}
+
+opt.termguicolors = true -- Enable true colors
+cmd.colorscheme("darcula-dark")
+
+-- colors overriding
+hi(0, "CursorLine", { bg = "#26282E", ctermbg = 1 })
+hi(0, "ColorColumn", { bg = "#393B40", ctermbg = 1 })
+hi(0, "Normal", { bg = "#1E2022", ctermbg = 1 })
+hi(0, "LineNr", { fg = "#474B54" })
+hi(0, "CursorLineNr", { fg = "#A0A2AA" })
+hi(0, "SignColumn", { fg = colors.red })
+
+-- gitsign
+hi(0, "GitGutterAdd", { fg = colors.green })
+hi(0, "GitGutterChange", { fg = colors.blue })
+hi(0, "GitGutterChangeDelete", { fg = colors.red })
+hi(0, "GitGutterDelete", { fg = colors.red })
+
+-- scrollbar
+hi(0, "ScrollbarGitAdd", { ctermfg = 0, fg = colors.green })
--- zsh/.zshenv
+++ zsh/.zshenv
@@ -1,4 +1,5 @@
 export EDITOR="nvim"
 export VISUAL="nvim"
 export XDG_CONFIG_HOME="$HOME/.config"
+export LG_CONFIG_FILE="$XDG_CONFIG_HOME/lazygit/config.yml,$XDG_CONFIG_HOME/lazygit/themes/jetbrains_dark.yml"
 
