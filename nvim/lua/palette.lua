bfae1e82b8daffbd6429293ff9e8a2f4c0c599df
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2024-09-10 16:36:53 +0300

    Add palette to neovim (#4)

---
+++ nvim/lua/palette.lua
@@ -0,0 +1,39 @@
+return {
+    editor = {
+        background = "#1E2022",
+        cursor_lne = "#26262E",
+        diagnostic = {
+            warn = "#F2C55C",
+            error = "#FA6675",
+        },
+        vcs = {
+            added_line = "#549159",
+            changed_line = "#375FAD",
+            deleted_line = "#868A91",
+        },
+    },
+    interface = {
+        text = "#DFE1E5",
+        hint_text = "#7A7E85",
+        background = "#2B2D30",
+        cursor_line = "#2E436E",
+        scroll_bar = "#9798",
+        cursor_line_inactive = "#43444A",
+        border = "#1E1F22", -- inactive element
+        active_element = "#3975F1", -- blue
+        popup_window = "#393B40",
+    },
+    lang = {
+        default = {
+            text = "#BCBEC4",
+            str = "#6AAB73",
+            keyword = "#CF8E6D",
+            digit = "#2AACB8",
+            function_decaration = "#56A8F5",
+            function_call = "#BCBEC4",
+            constant = "#C77DBB",
+            metadata = "#B3AE60",
+        },
+    },
+}
+
