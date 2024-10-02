0d0d6349005d2c7e7e91525e986c7a7cfe157886
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2024-10-02 10:40:54 +0300

    Add plugins (#8)

--- nvim/lua/darcula-dark/init.lua
+++ nvim/lua/darcula-dark/init.lua
@@ -1,9 +1,14 @@
 require("darcula-dark.theme")
+require("darcula-dark.treesitter")
+require("darcula-dark.lsp")
+require("darcula-dark.cmp")
+require("darcula-dark.gitsigns")
+require("darcula-dark.scrollbar")
 
 local lualine_theme = require("darcula-dark.lualine")
 require("lualine").setup {
     options = {
-        theme = lualine_theme 
+        theme = lualine_theme
     }
 }
 
