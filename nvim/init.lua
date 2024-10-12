363768b7e4e13b3010a6024652ceb97267d1093c
bost367 <24195348+bost367@users.noreply.github.com>
2024-10-12 18:26:20 +0300

    LSP Refactoring (#28)

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,12 +3,4 @@ require("config.options")
 require("config.keymaps")
 require("config.commands")
 require("darcula-dark")
-require("mason").setup()
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "rust_analyzer",
-    "bashls",
-  },
-})
 require("lsp")
