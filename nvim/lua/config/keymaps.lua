49eea05eef7b07238ba0948962415fee96b3acd7
bost367 <24195348+bost367@users.noreply.github.com>
2024-10-14 11:57:30 +0300

    Rremap buffer navigation (#33)

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,4 +1,4 @@
--- :help key-notation
+-- :h key-notation
 local map = vim.keymap.set
 local default_opts = { noremap = true, silent = true }
 
@@ -11,11 +11,11 @@ map("x", "gl", "$", default_opts)
 map("n", "<D-l>", ":Format <CR>", default_opts)
 
 -- bufferline
-map("n", "L", ":BufferLineCycleNext <CR>", default_opts)
-map("n", "H", ":BufferLineCyclePrev <CR>", default_opts)
+map("n", "<D-]>", ":BufferLineCycleNext <CR>", default_opts)
+map("n", "<D-[>", ":BufferLineCyclePrev <CR>", default_opts)
 map("n", "<D-w>", ":bd<CR>", default_opts)
 
 -- lsp mapping
-map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { noremap = true, silent = true })
-map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { noremap = true, silent = true })
-map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", { noremap = true, silent = true })
+map("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", default_opts)
+map("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", default_opts)
+map("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", default_opts)
