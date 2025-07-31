b6e4ed1e4b432905fb4e33a524ea95898a0b5060
Stepan Kruglov <kruglovstepan367@gmail.com>
2025-07-31 07:53:57 +0300

    Pointing treesitter to the main branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,35 +1,37 @@
--- Better syntax highlighting
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
+  build = ":TSUpdate",
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "markdown",
-        "markdown_inline",
-        "rust",
-        "kotlin",
-        "java",
-        "groovy",
-        "json",
-        "toml",
-        "yaml",
-        "xml",
-        "bash",
-        "go",
-        "proto",
-        "dockerfile",
-        "editorconfig",
-        "sql",
-        "just",
-      },
-      highlight = {
-        enable = true,
-      },
+    require("nvim-treesitter").install({
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "markdown",
+      "markdown_inline",
+      "rust",
+      "kotlin",
+      "java",
+      "json",
+      "toml",
+      "yaml",
+      "xml",
+      "bash",
+      "go",
+      "proto",
+      "dockerfile",
+      "editorconfig",
+      "sql",
+      "just",
+    })
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = { "*" },
+      callback = function()
+        vim.treesitter.start()
+      end,
     })
   end,
 }
