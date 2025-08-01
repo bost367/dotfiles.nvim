d241f366d402ae76d731f924863fb4cbe9208afb
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2025-08-01 09:11:26 +0300

    Enable LSP, formating and syntax highlighting for proto

---
+++ after/queries/proto/highlights.scm
@@ -0,0 +1,13 @@
+;; extends
+(map_field) @keyword
+(full_ident) @type
+(reserved
+    (field_names
+        (identifier) @type))
+(field_option
+    (identifier) @keyword
+    (#set! "priority" 300))
+(field_option
+    (constant
+        (full_ident
+            (identifier) @constant)))
