44b3a9b798c33d5827d76928393143396caca1e3
Stepan Kruglov <s.kruglov@tbank.ru>
2025-08-19 15:01:33 +0300

    (keymap) Set leader key explicity

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
+-- Setup <leader> key. Setup must be done before Lazy loading.
+vim.keymap.set("", "<Space>", "<Nop>") -- diable default <Space> behavior:
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("config.lazy")
 require("config.options")
 require("config.keymaps")
