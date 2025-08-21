e5290a350beaeb2971048a454966493e92db2d8c
Stepan Kruglov <kruglovstepan367@gmail.com>
2025-08-21 23:26:29 +0300

    blink

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,73 @@
+return {
+  "saghen/blink.cmp",
+  version = "1.*",
+  opts = {
+    keymap = {
+      preset = "none",
+      ["<C-Space>"] = { "show" },
+      ["<C-k>"] = { "select_prev" },
+      ["<C-j>"] = { "select_next" },
+      ["<CR>"] = { "accept", "fallback" },
+      -- ["???"] = { "scroll_documentation_up" },
+      -- ["??"] = { "scroll_documentation_down" },
+    },
+    completion = {
+      documentation = { auto_show = true },
+      list = {
+        selection = {
+          auto_insert = false,
+        },
+      },
+      menu = {
+        min_width = 65,
+        max_height = 15,
+        scrolloff = 2,
+        scrollbar = false,
+        draw = {
+          padding = 1,
+          columns = {
+            { "kind_icon", "label", "label_description", gap = 1 },
+            { "source_name" },
+          },
+        },
+      },
+    },
+    sources = {
+      default = { "lsp", "buffer" },
+      providers = {
+        lsp = { name = "lsp" },
+        buffer = { name = "buf" },
+        cmdline = { name = "cmd" },
+      },
+    },
+    appearance = {
+      kind_icons = {
+        Text = " ",
+        Method = "󰰑 ",
+        Function = "󰯼 ",
+        Constructor = " ",
+        Field = "󰯺 ",
+        Variable = "󰰚 ",
+        Class = "󰯳 ",
+        Interface = "󰯹 ",
+        Module = "󰰏  ",
+        Property = "󰰪 ",
+        Unit = " ",
+        Value = "󰎠 ",
+        Enum = "󰯷 ",
+        Keyword = "󰌋 ",
+        Snippet = " ",
+        Color = "󰏘 ",
+        File = " ",
+        Reference = " ",
+        Folder = " ",
+        EnumMember = "󰯷 ",
+        Constant = "󰯱 ",
+        Struct = "󰰡  ",
+        Event = " ",
+        Operator = "󰆕 ",
+        TypeParameter = "󰅲 ",
+      },
+    },
+  },
+}
