a478306eb15f5f1a04825ab185ba2b41471cb537
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2025-11-09 10:35:41 +0300

    (refactor) Refactor indent size

--- ftplugin/sh.lua
+++ ftplugin/sh.lua
@@ -1,4 +1,4 @@
-vim.opt_local.expandtab = true
-vim.opt_local.softtabstop = 2
-vim.opt_local.shiftwidth = 2
-vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true })
+local indent = 2
+vim.opt_local.tabstop = indent
+vim.opt_local.shiftwidth = indent
+vim.opt_local.softtabstop = indent
