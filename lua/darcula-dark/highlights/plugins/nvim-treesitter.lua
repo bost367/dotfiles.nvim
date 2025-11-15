29867dd9bda84f14ab524a6aeef27a9aafacadf1
Stepan Kruglov <s.kruglov@tbank.ru>
2025-11-15 13:33:43 +0300

    (theme) Fix and refactor

--- lua/darcula-dark/highlights/plugins/nvim-treesitter.lua
+++ lua/darcula-dark/highlights/plugins/nvim-treesitter.lua
@@ -4,23 +4,23 @@ local palette = require("darcula-dark.palette")
 ---@return table<string,Highliht>
 function M.groups()
   return {
-    ["@function"] = { fg = palette.syntax.default.function_decaration },
+    ["@function"] = { fg = palette.syntax.default.function_declaration },
     ["@function.call"] = { fg = palette.syntax.default.function_call },
     ["@function.builtin"] = { fg = palette.syntax.default.function_call },
-    ["@function.macro"] = { fg = palette.syntax.rust.macro },
-    ["@function.method"] = { fg = palette.syntax.default.function_decaration },
+    ["@function.macro"] = { fg = palette.syntax.default.metadata },
+    ["@function.method"] = { fg = palette.syntax.default.function_declaration },
     ["@function.method.call"] = { fg = palette.syntax.default.function_call },
     ["@constructor"] = { fg = palette.syntax.default.function_call },
     ["@operator"] = { fg = palette.syntax.default.foreground },
     ["@constant"] = { fg = palette.syntax.default.constant, italic = true },
     ["@constant.builtin"] = { fg = palette.syntax.default.constant, italic = true },
     ["@type"] = { fg = palette.syntax.default.foreground },
-    ["@type.builtin"] = { fg = palette.syntax.default.struct },
+    ["@type.builtin"] = { fg = palette.syntax.default.keyword },
     ["@module"] = { fg = palette.syntax.default.foreground },
     ["@module.builtin"] = { fg = palette.syntax.default.foreground },
     ["@label"] = { fg = palette.syntax.default.label },
     ["@keyword"] = { fg = palette.syntax.default.keyword },
-    ["@attribute"] = { fg = palette.syntax.rust.lifetime },
+    ["@attribute"] = { fg = palette.syntax.default.metadata },
     ["@property"] = { fg = palette.syntax.default.property },
     ["@boolean"] = { fg = palette.syntax.default.keyword },
     ["@keyword.function"] = { fg = palette.syntax.default.keyword },
@@ -40,28 +40,22 @@ function M.groups()
     ["@punctuation.bracket"] = { fg = palette.syntax.default.foreground },
     ["@comment"] = { fg = palette.syntax.default.comment },
     ["@comment.documentation"] = { fg = palette.syntax.default.comment_documentation },
-    ["@comment.todo"] = { fg = palette.syntax.default.comment_todo },
-    ["@spell.markdown"] = { fg = palette.syntax.default.foreground },
+    ["@comment.todo"] = { fg = palette.syntax.default.comment_todo, italic = true },
     ["@markup.italic"] = { fg = palette.syntax.default.foreground },
     ["@markup.strikethrough"] = { fg = palette.syntax.default.foreground },
     ["@markup.underline"] = { fg = palette.syntax.default.foreground },
     ["@markup.strong"] = { fg = palette.syntax.default.foreground },
-    ["@markup.heading"] = { fg = palette.syntax.default.constant },
-    ["@markup.heading.1"] = { fg = palette.syntax.default.constant },
-    ["@markup.heading.2"] = { fg = palette.syntax.default.constant },
-    ["@markup.heading.3"] = { fg = palette.syntax.default.constant },
-    ["@markup.heading.4"] = { fg = palette.syntax.default.constant },
-    ["@markup.heading.5"] = { fg = palette.syntax.default.constant },
-    ["@markup.heading.6"] = { fg = palette.syntax.default.constant },
+    ["@markup.heading"] = { fg = palette.syntax.default.constant, italic = true },
     ["@markup.quote"] = { fg = palette.syntax.default.string },
-    ["@markup.link"] = { fg = palette.link },
-    ["@markup.link.label"] = { fg = palette.link },
+    ["@markup.link"] = { fg = palette.link, underline = true },
+    ["@markup.link.label"] = { fg = palette.link, underline = true },
     ["@markup.link.url"] = { fg = palette.link },
     ["@markup.raw"] = { fg = palette.syntax.default.string },
     ["@markup.raw.block"] = { fg = palette.syntax.default.string },
     ["@markup.list"] = { fg = palette.syntax.default.keyword },
     ["@markup.list.checked"] = { fg = palette.syntax.default.keyword },
-    ["@markup.list.unckecked"] = { fg = palette.syntax.default.keyword },
+    ["@markup.list.unchecked"] = { fg = palette.syntax.default.keyword },
+    ["@conceal.markdown_inline"] = { fg = palette.syntax.default.keyword },
     ["@punctuation.special"] = { fg = palette.syntax.default.foreground },
     ["@tag"] = { fg = "#D5B778" },
     ["@tag.attribute"] = { fg = palette.syntax.default.foreground },
@@ -69,7 +63,7 @@ function M.groups()
     ["@diff.plus"] = { fg = palette.syntax.diff.added },
     ["@diff.minus"] = { fg = palette.syntax.diff.deleted },
     ["@diff.delta"] = { fg = palette.syntax.diff.changed },
-    ["StaticFunction"] = { fg = palette.syntax.default.function_decaration, italic = true },
+    ["StaticFunction"] = { fg = palette.syntax.default.function_declaration, italic = true },
 
     -- JSON
     ["@property.json"] = { fg = palette.syntax.default.constant },
@@ -79,23 +73,20 @@ function M.groups()
     ["@property.toml"] = { fg = palette.syntax.default.keyword },
 
     -- YAML
-    ["@label.yaml"] = { fg = palette.syntax.rust.macro },
+    ["@label.yaml"] = { fg = palette.syntax.rust.macros_functional },
     ["@comment.yaml"] = { fg = palette.syntax.default.comment_documentation },
     ["@punctuation.special.yaml"] = { fg = palette.syntax.default.foreground },
     ["@property.yaml"] = { fg = palette.syntax.default.keyword },
 
-    -- Markdow
+    -- Markdown
     ["@punctuation.special.markdown"] = { fg = palette.syntax.default.keyword },
 
     -- Bash
-    ["@function.call.bash"] = { fg = palette.syntax.bash.builtin },
-    ["@function.builtin.bash"] = { fg = palette.syntax.bash.builtin },
+    ["@function.call.bash"] = { fg = palette.syntax.bash.cli_call },
+    ["@function.builtin.bash"] = { fg = palette.syntax.bash.cli_call },
     ["@keyword.directive.bash"] = { fg = palette.syntax.default.foreground, bold = true },
     ["@punctuation.special.bash"] = { fg = palette.syntax.default.keyword },
     ["@variable.builtin.bash"] = { fg = palette.syntax.default.constant },
-
-    -- Kotlin
-    ["@attribute.kotlin"] = { fg = palette.syntax.default.metadata },
 
     -- XML
     ["@punctuation.delimiter.xml"] = { fg = palette.syntax.default.string },
@@ -103,9 +94,7 @@ function M.groups()
     -- Protobuf
     ["@string.special.proto"] = { fg = palette.syntax.default.string },
 
-    -- Rust
-    ["@function.macro.rust"] = { fg = palette.syntax.default.metadata },
-
+    -- Justfile
     ["@keyword.directive.just"] = { fg = palette.syntax.default.comment },
     ["@variable.just"] = { fg = palette.syntax.default.constant, italic = true },
     ["@function.just"] = { link = "StaticFunction" },
