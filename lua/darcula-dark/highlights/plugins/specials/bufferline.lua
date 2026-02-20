a78b1f037f493e7992c77b79ec7a28668889eccf
Stepan Kruglov <s.kruglov@tbank.ru>
2026-02-20 10:05:58 +0300

    bufferline

--- lua/darcula-dark/highlights/plugins/specials/bufferline.lua
+++ lua/darcula-dark/highlights/plugins/specials/bufferline.lua
@@ -1,11 +1,11 @@
 local palette = require("darcula-dark.palette")
 local not_selected = palette.editor.foreground
+local new_bg = "#25272A"
 
 -- Errros
 local ignore_diagnostics_hl = {
   fg = not_selected,
-  bg = palette.editor.background,
-  sp = palette.editor.separator,
+  bg = new_bg,
   underline = true,
   undercurl = false,
   italic = false,
@@ -14,8 +14,7 @@ local ignore_diagnostics_hl = {
 
 local select_ignored_diagnostic_hl = {
   fg = palette.interface.foreground,
-  bg = palette.editor.background,
-  sp = palette.editor.separator,
+  bg = new_bg,
   underline = true,
   undercurl = false,
   italic = false,
@@ -25,166 +24,118 @@ local select_ignored_diagnostic_hl = {
 return {
   -- empty space after buffers
   fill = {
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- buffers
   background = {
     fg = not_selected,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   buffer_visible = {
     fg = not_selected,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   buffer_selected = {
     fg = palette.interface.foreground,
     bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
     bold = false,
     italic = false,
   },
   -- separator among buffers
   separator = {
     fg = palette.editor.background,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   separator_visible = {
     fg = palette.editor.background,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   separator_selected = {
     fg = palette.editor.background,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- selected bufer
   indicator_visible = {
     fg = palette.interface.active_element,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   indicator_selected = {
     fg = palette.interface.active_element,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- number of hidden buffers (left/right arrows)
   trunc_marker = {
     fg = palette.icons.default,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- buffer close button
   close_button = {
     fg = palette.editor.background,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   close_button_visible = {
     fg = palette.hint_text,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   close_button_selected = {
     fg = palette.hint_text,
     bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
   },
   -- modified file indicator
   modified = {
     fg = palette.file_manager.vcs.modified_file,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   modified_visible = {
     fg = palette.file_manager.vcs.modified_file,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   modified_selected = {
     fg = palette.file_manager.vcs.modified_file,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- duplicated filenames in different folders
   duplicate = {
     fg = not_selected,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
     italic = false,
   },
   duplicate_selected = {
     fg = palette.interface.foreground,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
     italic = false,
   },
   duplicate_visible = {
     fg = not_selected,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
     italic = false,
   },
   -- tabs
   tab = {
     fg = not_selected,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   tab_selected = {
     fg = palette.interface.foreground,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   tab_separator = {
     fg = palette.editor.background,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   tab_separator_selected = {
     fg = palette.editor.background,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- buffer close button
   tab_close = {
     fg = not_selected,
-    bg = palette.editor.background,
-    sp = palette.editor.separator,
-    underline = true,
+    bg = new_bg,
   },
   -- errors
   error = {
     fg = not_selected,
-    bg = palette.editor.background,
+    bg = new_bg,
     sp = palette.editor.diagnostic.error,
     undercurl = true,
     italic = false,
@@ -192,7 +143,7 @@ return {
   },
   error_visible = {
     fg = not_selected,
-    bg = palette.editor.background,
+    bg = new_bg,
     sp = palette.editor.diagnostic.error,
     undercurl = true,
     italic = false,
@@ -200,7 +151,7 @@ return {
   },
   error_selected = {
     fg = palette.interface.foreground,
-    bg = palette.editor.background,
+    bg = new_bg,
     sp = palette.editor.diagnostic.error,
     undercurl = true,
     italic = false,
