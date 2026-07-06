bca345c8dde7f7e41578f1edc795c2173f2713bb
Stepan Kruglov <24195348+stepankv@users.noreply.github.com>
2026-07-06 20:13:24 +0300

    feat(mason): Remove Mason
    Mason canot work with nixos. All language servers installed via nixos
    configuration.

--- init.lua
+++ init.lua
@@ -7,3 +7,17 @@ require("config.lazy")
 require("config.options")
 require("config.keymaps")
 require("config.commands")
+
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/configs.md
+vim.lsp.enable({
+  "lua_ls",
+  "bashls",
+  "buf_ls",
+  "yamlls",
+  "jsonls",
+  "just",
+  "ts_ls",
+  "basedpyright",
+  "ruff",
+  "nil_ls",
+})
